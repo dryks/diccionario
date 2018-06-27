57f0979fbe272c6b6c9177efd5af378ee90729d2
dryks <ycenka@yandex.ru>
2018-06-27 13:38:32 +0300

    test

--- model/word.php
+++ model/word.php
@@ -1,13 +1,30 @@
 <?php
-
-class word{
+echo 100;
+class Word{
     public $wiki="Wikcionario:Portada";
 
 function __construct($wiki)
 {
-$this->wiki=wiki;
+$this->wiki=$wiki;
+}
+
+
+function getwiki($wiki){
+    
+    $input=file_get_contents("https://ru.wiktionary.org/w/api.php?action=parse&prop=text|headhtml|categories&format=json&mobileformat&redirects=1&useskin=minerva&page=".$wiki);
+    $texta=json_decode($input);
+return $texta;
+
 }
 
 
 
 }
+
+
+$cat=new Word("Wikcionario:Portada");
+
+$lom=$cat->getwiki("Москва");
+
+echo 222222;
+var_dump($lom);
