9e64c5b4ed0f1d3cf769f780825b41567827c05b
dryks <ycenka@yandex.ru>
2018-06-29 02:15:27 +0400

    test

--- model/word.php
+++ model/word.php
@@ -3,7 +3,7 @@ echo 100;
 $word="москва";
 class Word{
     public $wiki; //слово запроса
-    public $texta;//text body страницы
+    public $texta="100";//text body страницы
 
 function __construct($wiki="Заглавная_страница")
 {
@@ -15,15 +15,18 @@ $this->texta=$texta;
 function gettext($wiki="Заглавная_страница"){
     
     $input=file_get_contents("https://ru.wiktionary.org/w/api.php?action=parse&prop=text|headhtml|categories&format=json&mobileformat&redirects=1&useskin=minerva&page=".$wiki);
-    $texta=json_decode($input);
-    
+    $this->texta=json_decode($input);
+   
 return $texta;
 
 }
 
-function cleantexta(){
+
+function cleantext(){
+  return  $this->texta;
 
 }
+
 
 
 
@@ -34,5 +37,7 @@ $cat=new Word();
 
 $lom=$cat->gettext("Москва")->parse->headhtml->{'*'};
 $lom=$cat->gettext($word)->parse->text->{'*'}; 
+$clean=$cat->cleantext(); 
 echo 222222;
 var_dump($lom);
+//var_dump($clean);
