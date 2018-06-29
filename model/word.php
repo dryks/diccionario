8752a2d4f17100ce5a4e533875cdb3c3e297fc26
dryks <ycenka@yandex.ru>
2018-06-29 23:48:18 +0400

    test

--- model/word.php
+++ model/word.php
@@ -3,12 +3,14 @@ echo 100;
 $word="москва";
 class Word{
     public $wiki; //слово запроса
-    public $texta="100";//text body страницы
+    public $texta;//text body страницы
+    public $htmlhead ;
 
 function __construct($wiki="Заглавная_страница")
 {
 $this->wiki=$wiki;
 $this->texta=$texta;
+$this->htmlhead=$htmlhead;
 }
 
 
@@ -16,14 +18,21 @@ function gettext($wiki="Заглавная_страница"){
     
     $input=file_get_contents("https://ru.wiktionary.org/w/api.php?action=parse&prop=text|headhtml|categories&format=json&mobileformat&redirects=1&useskin=minerva&page=".$wiki);
     $this->texta=json_decode($input);
-   
-return $texta;
+    $inputdecode=json_decode($input);
+    $this->htmlhead=$inputdecode->parse->headhtml->{'*'};
+    $this->texta=$inputdecode->parse->text->{'*'};
+return $this->texta;
 
 }
 
 
 function cleantext(){
-  return  $this->texta;
+   $clean= $this->texta;
+   $clean = preg_replace("~<a .*>Править</a>~",'',$clean);
+
+
+
+  return  $clean;
 
 }
 
@@ -35,9 +44,76 @@ function cleantext(){
 
 $cat=new Word();
 
-$lom=$cat->gettext("Москва")->parse->headhtml->{'*'};
-$lom=$cat->gettext($word)->parse->text->{'*'}; 
+//$lom=$cat->gettext("Москва")->parse->headhtml->{'*'};
+$lom=$cat->gettext($word); 
 $clean=$cat->cleantext(); 
-echo 222222;
-var_dump($lom);
+//echo 222222;
+//var_dump($lom);
 //var_dump($clean);
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+     <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.1/cerulean/bootstrap.min.css" rel="stylesheet" integrity="sha384-0Mou2qXGeXK7k/Ue/a1hspEVcEP2zCpoQZw8/MPeUgISww+VmDJcy2ri9tX0a6iy" crossorigin="anonymous">
+    <style>
+
+        body {
+  background-image: url("http://diccionario.pro/img/light.png");
+  background-repeat: repeat;
+}
+  
+</style>
+     <title>Hello, world!</title>
+  </head>
+
+
+
+  <body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+<?php echo $clean ?>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
+
