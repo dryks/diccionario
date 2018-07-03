a2eebfb97111ac7d52aee8ac2fdd7a10361ebb20
dryks <ycenka@yandex.ru>
2018-07-04 02:31:14 +0400

    3,07

--- controller/word.php
+++ controller/word.php
@@ -12,3 +12,6 @@ $wordclean=$wordstart->cleantext();
 echo $wordstart->htmlhead; 
 echo $wordstart->texta;
 include "view/footer.php";
+
+//парсинг меню вики
+//https://ru.wiktionary.org/w/api.php?action=parse&format=json&prop=sections&page=%D0%BC%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&redirects&
