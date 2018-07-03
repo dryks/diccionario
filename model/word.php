355a9c4b4323b6b51e66717d71a93c443377d1bb
dryks <ycenka@yandex.ru>
2018-07-04 00:07:25 +0400

    test

--- model/word.php
+++ model/word.php
@@ -32,7 +32,7 @@ function cleantext(){
 //форматирование head к выводу
 //стили вывод в хеад
 $headend=' <!-- Bootstrap CSS -->
-<link href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.1/cerulean/bootstrap.min.css" rel="stylesheet" integrity="sha384-0Mou2qXGeXK7k/Ue/a1hspEVcEP2zCpoQZw8/MPeUgISww+VmDJcy2ri9tX0a6iy" crossorigin="anonymous">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.1/cerulean/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
 <style>
 
    body {
@@ -41,41 +41,9 @@ background-repeat: repeat;
 }
 
 </style>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </head>';
-//вывод в боди после тега body
-$navig='
-<div class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarColor01">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search">
-      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</div>';
-
+include"view/menu.php";
 
 $cleanhead = preg_replace("#</head>#",$headend,$cleanhead);
 $cleanhead = preg_replace("#<title>(.*)</title>#","<title>{$this->title}</title>",$cleanhead);
@@ -97,11 +65,11 @@ $cleanhead = preg_replace("#<body (.*?)>#","<body $1>$navig",$cleanhead);
 }
 
 
-$cat=new Word();
+$wordstart=new Word();
 
 //$lom=$cat->gettext("Москва")->parse->headhtml->{'*'};
-$lom=$cat->gettext($word); 
-$clean=$cat->cleantext(); 
+$wordget=$wordstart->gettext($word); 
+$wordclean=$wordstart->cleantext(); 
 //echo 222222;
 //var_dump($lom);
 //var_dump($clean);
@@ -129,7 +97,7 @@ $clean=$cat->cleantext();
       </head>
 */?>
 
-<?php echo $cat->htmlhead; ?>
+<?php echo $wordstart->htmlhead; ?>
 
 
  
@@ -167,18 +135,10 @@ $clean=$cat->cleantext();
 </nav>
 */?>
 
-<?php echo  $cat->texta; ?>
+<?php echo $wordstart->texta; ?>
 <?php
-function get_footer(){
-  ob_start();
 include "view/footer.php";
 
-
-return ob_get_clean();
-echo "199";
-
-}; 
-echo get_footer();
 
 ?>
 
