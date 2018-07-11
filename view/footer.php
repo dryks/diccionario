48337b8cdbd7b15fa80e4f5380a031505b215015
dryks <ycenka@yandex.ru>
2018-07-11 14:09:54 +0400

    11,07

--- view/footer.php
+++ view/footer.php
@@ -1,24 +1,27 @@
-
- </div>
-            </div>
-        </div>
-
+</div>
+</div>
+</div>
 
 <!-- content-wrapper ends -->
 <footer class="footer">
-    <div class="container-fluid clearfix">
-        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">CC © 2011-2018 <a href="https://diccionario.pro" target="_blank">diccionario.pro</a>. Копирование запрещено.</span>
-          </div>
+<div class="container-fluid clearfix">
+<span class="text-muted d-block text-center text-sm-left d-sm-inline-block">CC © 2011-2018
+<a href="https://diccionario.pro" target="_blank">diccionario.pro</a>. Копирование запрещено.</span>
+</div>
 </footer>
-
-
 
 <!-- Plugin js for this page-->
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
+<script
+src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+crossorigin="anonymous"></script>
+<script
+src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+crossorigin="anonymous"></script>
+<script
+src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.min.js"
+crossorigin="anonymous"></script>
 </body>
 </html>
-
