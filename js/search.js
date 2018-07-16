896fc04e0de256abc12a15289aa264c672bb80c0
dryks <ycenka@yandex.ru>
2018-07-17 02:18:31 +0400

    17,07

---
+++ js/search.js
@@ -0,0 +1,37 @@
+ <script>
+                                var options = {
+
+                                    url: function(phrase) {
+                                        if (phrase !== "") {
+                                            return "https://diccionario.pro/site.php?text=" + phrase + "";
+                                        } else {
+
+                                            return "https://diccionario.pro/site.php?text=0";
+                                        }
+                                    },
+
+
+
+
+
+
+                                    getValue: "text",
+
+                                    template: {
+                                        type: "links",
+                                        fields: {
+                                            link: "site"
+                                        }
+                                    },
+
+                                    list: {
+                                        match: {
+                                            enabled: false
+                                        }
+                                    },
+
+                                    theme: "Square"
+                                };
+
+                                $("#data-links").easyAutocomplete(options);
+                            </script>
